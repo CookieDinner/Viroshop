88149935c36bc981f2a55d0a32b3faedde3ab027
Damian Wędzikowski <dwedzikowski@gmail.com>
2020-11-29 21:23:54 +0100

    Update main.dart
    Changed back to dark theme, because why not

--- PPI_MOBILE/viroshop/lib/main.dart
+++ PPI_MOBILE/viroshop/lib/main.dart
@@ -14,7 +14,7 @@ void main() async{
   final tempDbPath = await getApplicationDocumentsDirectory();
   Data().dbPath = path.join(dbPath.path, 'local_db.sqlite');
   Data().tempDbPath = tempDbPath.path;
-  CustomTheme().setTheme(true);
+  CustomTheme().setTheme(false);
   runApp(App());
 }
 
