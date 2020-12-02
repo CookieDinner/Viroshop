a4d808c8f37bffa1c57bfb169f4b8c6c4ac63355
dj-chemik <dj-chemik@wp.pl>
2020-12-02 14:29:43 +0100

    [PPI-38] Add background color

--- PPI_MOBILE/viroshop/lib/Views/ZbikPaymentView.dart
+++ PPI_MOBILE/viroshop/lib/Views/ZbikPaymentView.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:viroshop/Utilities/CustomTheme.dart';
 import 'package:viroshop/Utilities/Util.dart';
 import 'package:viroshop/Views/NavigationViews/NavigationViewTemplate.dart';
 import 'package:viroshop/Views/StoreNavigationView.dart';
@@ -14,11 +15,13 @@ class _ZbikPaymentViewState extends State<ZbikPaymentView> {
   @override
   Widget build(BuildContext context) {
     final mediaSize = Util.getDimensions(context);
-
     return Container(
       height: mediaSize.height,
       width: mediaSize.width,
+      color: new Color.fromARGB(200, 60, 210, 50),
+      alignment: Alignment.center,
       child: Text(
+
         "System płatności ŻBIK"
       )
     );
