9aec197dda77dab38f0476b45068ff1711fbc196
Chemik <dj-chemik@wp.pl>
2021-01-09 21:55:57 +0100

    [PPI-63] Make endpoint which respond alleys with selected products

--- PPI_BACKEND/viroshop/src/main/java/pl/put/poznan/viroshop/api/ShopApi.java
+++ PPI_BACKEND/viroshop/src/main/java/pl/put/poznan/viroshop/api/ShopApi.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pl.put.poznan.viroshop.dao.entities.AlleyEntity;
 import pl.put.poznan.viroshop.dao.entities.ShopEntity;
+import pl.put.poznan.viroshop.manager.AlleyManager;
 import pl.put.poznan.viroshop.manager.ShopManager;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -32,6 +36,16 @@ public class ShopApi {
     @GetMapping(value = "/api/shops/area", produces = "application/json; charset=UTF-8")
     public Iterable<ShopEntity> getSurroundingShops(@RequestParam String city) {
         return shopManager.findAllFromCity(city);
+    }
+
+    @GetMapping(value = "/api/shop/places", produces = "application/json; charset=UTF-8")
+    public Iterable<AlleyEntity> getAlleysWithSelectedProducts(@RequestParam Long shopId, @RequestParam List<Long> productIds) {
+        return shopManager.getAlleysWithSelectedProducts(shopId, productIds);
+    }
+
+    @GetMapping(value = "/api/shop/shortway", produces = "application/json; charset=UTF-8")
+    public Iterable<AlleyEntity> getShortestWay(@RequestParam Long shopId, @RequestParam List<Long> productIds) {
+        return shopManager.getShortestWay(shopId, productIds);
     }
 
 }
--- PPI_BACKEND/viroshop/src/main/java/pl/put/poznan/viroshop/manager/ShopManager.java
+++ PPI_BACKEND/viroshop/src/main/java/pl/put/poznan/viroshop/manager/ShopManager.java
@@ -2,9 +2,16 @@ package pl.put.poznan.viroshop.manager;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.put.poznan.viroshop.dao.entities.AlleyEntity;
+import pl.put.poznan.viroshop.dao.entities.ProductEntity;
 import pl.put.poznan.viroshop.dao.entities.ShopEntity;
 import pl.put.poznan.viroshop.dao.repositories.ShopRepo;
 
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -13,10 +20,12 @@ import java.util.stream.StreamSupport;
 public class ShopManager {
 
     private final ShopRepo shopRepo;
+    private final AlleyManager alleyManager;
 
     @Autowired
-    public ShopManager(ShopRepo shopRepo) {
+    public ShopManager(ShopRepo shopRepo, AlleyManager alleyManager) {
         this.shopRepo = shopRepo;
+        this.alleyManager = alleyManager;
     }
 
     public Optional<ShopEntity> findOneById(Long id) {
@@ -42,4 +51,24 @@ public class ShopManager {
         shopRepo.deleteById(id);
     }
 
+    public Iterable<AlleyEntity> getAlleysWithSelectedProducts(Long shopId, List<Long> productIds) {
+        Iterable<AlleyEntity> allAlleys = alleyManager.findAll();
+        ArrayList<AlleyEntity> shopAlleys = new ArrayList<>();
+        ArrayList<AlleyEntity> alleysWithSelectedProducts = new ArrayList<>();
+        allAlleys.forEach(alley -> {
+            if (alley.getShopEntity().getId() == shopId) {
+                shopAlleys.add(alley);
+            }
+        });
+        productIds.forEach(productId -> {
+            shopAlleys.forEach(alley -> {
+                Optional<ProductEntity> foundProduct = alley.getProducts().stream().filter(product -> product.getId() == productId).findFirst();
+                if (!foundProduct.isEmpty()) {
+                    alleysWithSelectedProducts.add(alley);
+                }
+
+            });
+        });
+        return alleysWithSelectedProducts;
+    }
 }
