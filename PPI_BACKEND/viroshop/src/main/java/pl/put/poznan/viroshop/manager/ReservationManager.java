a58c2af32ae25891e3732c928bcc74cdc1d37101
Chemik <dj-chemik@wp.pl>
2021-01-19 22:58:26 +0100

    [PPI-66] Fix reservations

--- PPI_BACKEND/viroshop/src/main/java/pl/put/poznan/viroshop/manager/ReservationManager.java
+++ PPI_BACKEND/viroshop/src/main/java/pl/put/poznan/viroshop/manager/ReservationManager.java
@@ -231,14 +231,14 @@ public class ReservationManager {
     private long getReservationsDayCount(LocalDate date, ShopEntity shopEntity) {
         return StreamSupport.stream(reservationRepo.findAll().spliterator(), false)
                 .filter(res -> res.getShop().getId() == shopEntity.getId())
-                .filter(res -> res.getDate() == date)
+                .filter(res -> res.getDate().equals(date))
                 .count();
     }
 
     private long getReservationsCountForQuarter(LocalDate date, int quarter, ShopEntity shopEntity) {
-        return StreamSupport.stream(reservationRepo.findAll().spliterator(), false)
+         return StreamSupport.stream(reservationRepo.findAll().spliterator(), false)
                 .filter(res -> res.getShop().getId() == shopEntity.getId())
-                .filter(res -> res.getDate() == date)
+                .filter(res -> res.getDate().equals(date))
                 .filter(res -> res.getQuarterOfDay() == quarter)
                 .count();
     }
